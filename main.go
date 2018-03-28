f6fe427d0f56333a469eb05c022603e79654dacf
Krishna Srinivas <krishna.srinivas@gmail.com>
2018-03-28 00:21:39 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/rakyll/statik/fs"
+
+	_ "./statik" // TODO: Replace with the absolute import path
+)
+
+func main() {
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/", http.FileServer(statikFS))
+	http.ListenAndServe(":8080", nil)
+}
